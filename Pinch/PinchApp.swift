a462c4b65dcba04abaec8148c5fd7f03949d62c9
Shankar Kakumani <shankarchowdary7545@gmail.com>
2022-06-04 16:55:05 +0530

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Shankar Chowdary on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
